f4a8482d7a35fa7a784f7f26cca557c0c613a295
koraphat <koraphat.lum@gmail.com>
2024-10-24 17:56:44 +0700

    first submit

--- src/components/BookingList.tsx
+++ src/components/BookingList.tsx
@@ -20,16 +20,16 @@ export default function BookingList() {
   return (
     <div className="flex flex-col space-y-[24px] px-[32px] py-[32px]">
       {bookItems.map((item) => (
-        <div className="flex flex-row justify-between bg-lime-100 px-[16px] py-[16px] w-full border-[2px]  border-lime-950 rounded-[24px]">
+        <div className="flex flex-row justify-between bg-white px-[16px] py-[16px] w-full border-[2px]  border-lime-950 rounded-[24px]">
           <div
             className="flex flex-col space-y-[8px] text-lime-900"
             key={item.id}
           >
-            <div className="text-[16px] flex flex-row items-center space-x-[8px]">
+            <div className="text-[24px] font-bold flex flex-row items-center space-x-[8px]">
               <div>{item.name}</div>
               <div>{item.surname}</div>
             </div>
-            <div className="text-[16px]">{item.id}</div>
+            <div className="text-[16px] ">{item.id}</div>
             <div className="text-[16px]">{item.hospital}</div>
             <div className="text-[24px]">{item.bookDate}</div>
           </div>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,8 +2,58 @@ import { configureStore } from "@reduxjs/toolkit";
 import bookSlice from "./features/bookSlice";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
 
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import createWebStorage from "redux-persist/es/storage/createWebStorage";
+import { WebStorage } from "redux-persist";
+import { combineReducers } from "redux";
+
+function createPersistStorage(): WebStorage {
+  const isServer = typeof window === "undefined";
+  if (isServer) {
+    return {
+      getItem() {
+        return Promise.resolve(null);
+      },
+      setItem() {
+        return Promise.resolve();
+      },
+      removeItem() {
+        return Promise.resolve();
+      },
+    };
+  }
+  return createWebStorage("local");
+}
+
+const storage = createPersistStorage();
+
+const PersistConfig = {
+  key: "rootPersist",
+  storage, // Use the storage module from redux-persist
+};
+
+const rootReducer = combineReducers({
+  bookSlice,
+});
+
+const reduxPersistReducer = persistReducer(PersistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: { bookSlice },
+  reducer: reduxPersistReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
